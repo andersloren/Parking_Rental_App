6df381937637b628c25889de9ff3f13ff190d2c8
Liebniz <anders.leif.loren@liebniz.com>
2023-08-30 11:45:51 +0200

    Started with Singleton Design Pattern, created Dao and DaoImpl for all models (except Vehicle Type)

--- src/test/java/lexicon/data/impl/ParkingSpotDaoImplTest.java
+++ src/test/java/lexicon/data/impl/ParkingSpotDaoImplTest.java
@@ -3,6 +3,6 @@ package lexicon.data.impl;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingSpotDaoImplTest {
-    // todo: implement unit test for this class
+    /* todo: implement unit test for this class */
 
 }
--- src/test/java/lexicon/data/impl/ReservationDaoImplTest.java
+++ src/test/java/lexicon/data/impl/ReservationDaoImplTest.java
@@ -3,6 +3,6 @@ package lexicon.data.impl;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationDaoImplTest {
-    //todo: implement unit test for this case
+    /* todo: implement unit test for this case */
 
 }
